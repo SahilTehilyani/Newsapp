73f598de1adc643d47103ae5cb39b7af2c12574b
SahilTehilyani <167900474+SahilTehilyani@users.noreply.github.com>
2024-11-18 18:15:05 +0530

    first changes

---
+++ src/components/NewsItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <img src={!imageUrl ? "https://techcrunch.com/wp-content/uploads/2024/05/Screenshot-2024-05-08-at-7.19.50%E2%80%AFAM.jpg?resize=1200,919" : imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
